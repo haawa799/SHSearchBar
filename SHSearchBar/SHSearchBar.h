34c599075f9e888801971709fc4a1c1807ef5fd7
Andrew Kharchyshyn <haawaplus@gmail.com>
2017-05-25 13:08:40 +0300

    Carthage support

---
+++ SHSearchBar/SHSearchBar.h
@@ -0,0 +1,19 @@
+//
+//  SHSearchBar.h
+//  SHSearchBar
+//
+//  Created by Andrii Kharchyshyn on 5/25/17.
+//  Copyright © 2017 @haawa799. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHSearchBar.
+FOUNDATION_EXPORT double SHSearchBarVersionNumber;
+
+//! Project version string for SHSearchBar.
+FOUNDATION_EXPORT const unsigned char SHSearchBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHSearchBar/PublicHeader.h>
+
+
